3f9024f187935927ba431f19cc821e4bf7258c9f
Tom Hören <horen.tom@gmail.com>
2021-09-06 11:16:39 +0200

    Update maxiconda.py

--- scripts/maxiconda.py
+++ scripts/maxiconda.py
@@ -515,8 +515,7 @@ target_platform: {self.subdir}
         PY = package_name_parts[-1].split(".")[0]
         version = package_name_parts[-2]
         environment = "-".join(package_name_parts[:-2])
-        print(f"package_name='{package_name}' --> environment='{environment}', version='{version}', PY='{PY}'")                          
-            
+
         print(f"Uploading : '{package_fpath}' ... ", end="", flush=True)
         if self.is_uploadable(package_fpath):
             retval = True
